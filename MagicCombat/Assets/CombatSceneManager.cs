9716463dca4bfcef0e7302de6813c08a92f15fbe
Monipulateddusk <98320488+Monipulateddusk@users.noreply.github.com>
2024-05-28 15:30:53 +0100

    Current Progress

---
+++ MagicCombat/Assets/CombatSceneManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatSceneManager : MonoBehaviour
+{
+    List<GameObject> enemyGameObjects = new List<GameObject>();
+    List<GameObject> playerGameObjects = new List<GameObject>();
+
+    GameObject[] focusedPlayerObjects;
+
+}
