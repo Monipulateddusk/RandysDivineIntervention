92337871b6a111d235b138d7e970339b59144499
Monipulateddusk <98320488+Monipulateddusk@users.noreply.github.com>
2026-03-08 17:39:17 +0000

    Update UserInterfaceManager.cs

--- MagicCombat/Assets/_Scripts/BattleScripts/Turn-Based Combat Scripts/UserInterfaceManager.cs
+++ MagicCombat/Assets/_Scripts/BattleScripts/Turn-Based Combat Scripts/UserInterfaceManager.cs
@@ -169,7 +169,7 @@ public class UserInterfaceManager : MonoBehaviour
         if (this.selectedUserInterfaceElement.hoveredUIObject != null)
         {
             /*  If we are selecting a DialogueBox, we want to pull it to the front. */
-            if ((DialogueBoxBehaviour)selectedUserInterfaceElement.hoveredUIObject)
+            if (selectedUserInterfaceElement.hoveredUIObject is DialogueBoxBehaviour && selectedUserInterfaceElement.hoveredUIObject as DialogueBoxBehaviour != null)
             {
                 ((DialogueBoxBehaviour)this.selectedUserInterfaceElement.hoveredUIObject).transform.SetAsLastSibling();
             }
